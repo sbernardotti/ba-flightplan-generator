87749e3fde158bdb54aa16100b9a8a7105f605b0
Sebastian Bernardotti <sebastian.bernardotti@gmail.com>
2021-04-29 00:53:23 -0300

    ba-flightplan-generator

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar level has-shadow is-fullwidth py-2">
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="title">Plan de vuelo</p>
+      </div>
+    </div>
+    <div class="level-item has-text-centered">
+      <img src="./assets/logo.webp" alt="BA FLIGHT" width="88" height="45" />
+    </div>
+  </nav>
+  <div class="container px-4">
+    <FlightPlanForm class="my-4" />
+  </div>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        &copy; 2021 - Escuela de vuelo
+        <strong><a href="https://www.ba-flight.com/" target="_blank">BA FLIGHT</a></strong
+        >.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+import FlightPlanForm from "./components/FlightPlanForm.vue";
+
+export default {
+  name: "App",
+  components: {
+    FlightPlanForm,
+  },
+};
+</script>
---
+++ src/data/flightRules.js
@@ -0,0 +1,6 @@
+export default [
+  { id: "I", description: "Instrumental" },
+  { id: "V", description: "Visual" },
+  { id: "Y", description: "Inicialmente instrumental" },
+  { id: "Z", description: "Inicialmente visual" },
+];
